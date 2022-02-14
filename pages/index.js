fd06684eee78af57b08c4f91fd7a6e94ce71be15
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-02-15 00:05:48 +0200

    change Home.module.css

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
-        <h1>Homepage</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt distinctio dolore,
+        <h1 className={styles.title}>Homepage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt distinctio dolore,
             facere illum maxime molestias rerum tempora vitae? A alias cumque debitis eaque itaque labore,
             repudiandae totam vero!</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt distinctio dolore,
+        <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt distinctio dolore,
             facere illum maxime molestias rerum tempora vitae? A alias cumque debitis eaque itaque labore,
             repudiandae totam vero!</p>
         <Link href='/ninjas'>
-            <a>See Ninja Listing</a>
+            <a className={styles.btn}>See Ninja Listing</a>
         </Link>
     </div>
   )
