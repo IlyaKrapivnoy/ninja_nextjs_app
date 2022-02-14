3bad8670ca1eba3a03aaff26cfd04687a418a748
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-02-15 00:30:40 +0200

    add logo

--- pages/404.js
+++ pages/404.js
@@ -13,7 +13,7 @@ const NonFount = () => {
     }, [router])
     return (
         <div className='not-found'>
-            <h1>Oooops...</h1>
+            <h1>Oops...</h1>
             <h2>That page cannot be found</h2>
             <p>Go back to the <Link href="/"><a>Homepage</a></Link></p>
         </div>
