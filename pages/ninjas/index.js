77ff51bb54f6f8db6e0766db4e636b2c46a76d10
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-02-15 00:46:20 +0200

    fetch ninjas

--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,18 +1,34 @@
-import React from 'react';
-import Head from 'next/head';
+import React from "react";
+import Head from "next/head";
 
-const Index = () => {
-    return (
-        <>
-            <Head>
-                <title>Ninja List | Total Ninjas</title>
-                <meta name='keywords' content='ninjas' />
-            </Head>
-        <div>
-            <h1>Total Ninjas</h1>
-        </div>
-        </>
-    );
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data },
+  };
+};
+
+const Index = ({ ninjas }) => {
+  return (
+    <>
+      <Head>
+        <title>Ninja List | Total Ninjas</title>
+        <meta name="keywords" content="ninjas" />
+      </Head>
+      <div>
+        <h1>Total Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Index;
