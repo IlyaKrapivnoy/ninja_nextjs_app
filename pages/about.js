d50d0daf796e6487beb24c91d6dc32d791a1a76e
IlyaKrapivnoy <ilya.krapivnoy@gmail.com>
2022-02-14 23:44:43 +0200

    create navbar

---
+++ pages/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt distinctio dolore,
+                facere illum maxime molestias rerum tempora vitae? A alias cumque debitis eaque itaque labore,
+                repudiandae totam vero!</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt distinctio dolore,
+                facere illum maxime molestias rerum tempora vitae? A alias cumque debitis eaque itaque labore,
+                repudiandae totam vero!</p>
+        </div>
+    );
+};
+
+export default About;
