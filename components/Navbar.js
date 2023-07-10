69f7ba8769cb8d322d1287d65023ac7a4bc49542
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-10 21:12:58 +0100

    fix logo

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import Logo from '../public/logo.png';
+import styles from '../styles/home.module.scss';
 
 const Navbar = () => {
     return (
-        <nav>
-            <div className='logo'>
-                <Image src="/logo.png" width={128} height={77} alt={'logo'} />
+        <nav className={styles.nav}>
+            <div className="logo">
+                <Link href="/">
+                    <Image src={Logo} width="100%" height="100%" alt="logo" />
+                </Link>
             </div>
-            <Link href='/'><a>Home</a></Link>
-            <Link href='/about'><a>About</a></Link>
-            <Link href='/ninjas'><a>Ninja List</a></Link>
+            <div className="navItems">
+                <Link href="/">
+                    <a>Home</a>
+                </Link>
+                <Link href="/about">
+                    <a>About</a>
+                </Link>
+                <Link href="/ninjas">
+                    <a>Ninja List</a>
+                </Link>
+            </div>
         </nav>
     );
 };
