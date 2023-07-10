38cd78de8170b0abd34ac2eb7b028ab743f1ba89
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-10 18:47:58 +0100

    adicione scss

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/Home.module.scss';
 import Head from 'next/head';
 
 export default function Home() {
