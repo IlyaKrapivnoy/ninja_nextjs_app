d28b4244035284a79ad118c1137b9b9b1fda622c
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-10 18:35:22 +0100

    adicione prettier

--- pages/index.js
+++ pages/index.js
@@ -4,24 +4,30 @@ import styles from '../styles/Home.module.css';
 import Head from 'next/head';
 
 export default function Home() {
-  return (
-      <>
-        <Head>
-            <title>Ninja List | Home</title>
-            <meta name='keywords' content='ninjas' />
-        </Head>
-        <div>
-            <h1 className={styles.title}>Homepage</h1>
-            <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt distinctio dolore,
-                facere illum maxime molestias rerum tempora vitae? A alias cumque debitis eaque itaque labore,
-                repudiandae totam vero!</p>
-            <p className={styles.text}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Atque delectus deserunt distinctio dolore,
-                facere illum maxime molestias rerum tempora vitae? A alias cumque debitis eaque itaque labore,
-                repudiandae totam vero!</p>
-            <Link href='/ninjas'>
-                <a className={styles.btn}>See Ninja Listing</a>
-            </Link>
-        </div>
-      </>
-  )
+    return (
+        <>
+            <Head>
+                <title>Ninja List | Home</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1 className={styles.title}>Homepage</h1>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque delectus deserunt distinctio dolore, facere illum
+                    maxime molestias rerum tempora vitae? A alias cumque debitis
+                    eaque itaque labore, repudiandae totam vero!
+                </p>
+                <p className={styles.text}>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque delectus deserunt distinctio dolore, facere illum
+                    maxime molestias rerum tempora vitae? A alias cumque debitis
+                    eaque itaque labore, repudiandae totam vero!
+                </p>
+                <Link href="/ninjas">
+                    <a className={styles.btn}>See Ninja Listing</a>
+                </Link>
+            </div>
+        </>
+    );
 }
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,36 +1,36 @@
-import React from "react";
-import Head from "next/head";
-import styles from "../../styles/Ninjas.module.css";
-import Link from "next/link";
+import React from 'react';
+import Head from 'next/head';
+import styles from '../../styles/Ninjas.module.css';
+import Link from 'next/link';
 
 export const getStaticProps = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const data = await res.json();
 
-  return {
-    props: { ninjas: data },
-  };
+    return {
+        props: { ninjas: data }
+    };
 };
 
 const Index = ({ ninjas }) => {
-  return (
-    <>
-      <Head>
-        <title>Ninja List | Total Ninjas</title>
-        <meta name="keywords" content="ninjas" />
-      </Head>
-      <div>
-        <h1>Total Ninjas</h1>
-        {ninjas.map((ninja) => (
-          <Link href={"/ninjas/" + ninja.id} key={ninja.id}>
-            <a className={styles.single}>
-              <h3>{ninja.name}</h3>
-            </a>
-          </Link>
-        ))}
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Ninja List | Total Ninjas</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>Total Ninjas</h1>
+                {ninjas.map((ninja) => (
+                    <Link href={'/ninjas/' + ninja.id} key={ninja.id}>
+                        <a className={styles.single}>
+                            <h3>{ninja.name}</h3>
+                        </a>
+                    </Link>
+                ))}
+            </div>
+        </>
+    );
 };
 
 export default Index;
