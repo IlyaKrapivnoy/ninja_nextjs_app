ca2b0a236a6c0df8a9403e94dc5d7489a87a421b
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-12 13:21:39 +0100

    change About structure

---
+++ components/web-pages/About/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import Head from 'next/head';
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+                <meta name="keywords" content="ninjas" />
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque delectus deserunt distinctio dolore, facere illum
+                    maxime molestias rerum tempora vitae? A alias cumque debitis
+                    eaque itaque labore, repudiandae totam vero!
+                </p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Atque delectus deserunt distinctio dolore, facere illum
+                    maxime molestias rerum tempora vitae? A alias cumque debitis
+                    eaque itaque labore, repudiandae totam vero!
+                </p>
+            </div>
+        </>
+    );
+};
+
+export default About;
