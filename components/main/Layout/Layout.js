f9e5e5d14c080653a9690d8b4403196543f1c997
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-14 17:30:01 +0100

    position footer to the bottom

--- components/main/Layout/Layout.js
+++ components/main/Layout/Layout.js
@@ -5,10 +5,12 @@ import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
     return (
-        <div className={styles.content}>
-            <Navbar />
-            {children}
-            <Footer />
+        <div className={styles.container}>
+            <Navbar role="navigation" aria-label="Main Navigation" />
+            <main role="main" className={styles.main}>
+                {children}
+            </main>
+            <Footer role="contentinfo" aria-label="Footer" />
         </div>
     );
 };
