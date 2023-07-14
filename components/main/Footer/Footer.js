e2d4d30958b4511f849cb9d0872e3ed6f0623821
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-14 17:11:51 +0100

    separate footer styles

--- components/main/Footer/Footer.js
+++ components/main/Footer/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Footer.module.scss';
 
 const Footer = () => {
     const currentYear = new Date().getFullYear();
 
-    return <footer>&copy; Copyright {currentYear} Ilya Krapivnoy</footer>;
+    return (
+        <footer className={styles.footer}>
+            &copy; Copyright {currentYear} Ilya Krapivnoy
+        </footer>
+    );
 };
 
 export default Footer;
