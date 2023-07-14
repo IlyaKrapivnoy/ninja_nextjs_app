532fc8958a12ca2a31220acca14fc98311724c4e
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-14 17:14:08 +0100

    separate layout styles

--- components/main/Layout/Layout.js
+++ components/main/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '../Navbar/Navbar';
 import Footer from '../Footer/Footer';
+import styles from './Layout.module.scss';
 
 const Layout = ({ children }) => {
     return (
-        <div className="content">
+        <div className={styles.content}>
             <Navbar />
             {children}
             <Footer />
