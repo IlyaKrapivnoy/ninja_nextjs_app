429c34988cf333887335f5aa79c1fa8070536a07
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-14 16:02:56 +0100

    add active navbar link

--- components/main/Navbar/Navbar.js
+++ components/main/Navbar/Navbar.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
+import { useRouter } from 'next/router';
+import cn from 'classnames';
 import styles from './Navbar.module.scss';
 import Logo from '../../../public/logo.png';
 
 const Navbar = () => {
+    const router = useRouter();
+
+    const navigationLinks = [
+        { path: '/', label: 'Home' },
+        { path: '/about', label: 'About' },
+        { path: '/ninjas', label: 'Ninja List' }
+    ];
+
     return (
         <nav className={styles.nav}>
             <div className={styles.logo}>
@@ -12,22 +22,20 @@ const Navbar = () => {
                     <Image src={Logo} width="100" alt="logo" />
                 </Link>
             </div>
+
             <ul className={styles.navItems}>
-                <li className={styles.navItem}>
-                    <Link href="/" className={styles.navLink}>
-                        Home
-                    </Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link href="/about" className={styles.navLink}>
-                        About
-                    </Link>
-                </li>
-                <li className={styles.navItem}>
-                    <Link href="/ninjas" className={styles.navLink}>
-                        Ninja List
-                    </Link>
-                </li>
+                {navigationLinks.map((link) => (
+                    <li key={link.path} className={styles.navItem}>
+                        <Link
+                            href={link.path}
+                            className={cn(styles.navLink, {
+                                [styles.active]: router.pathname === link.path
+                            })}
+                        >
+                            {link.label}
+                        </Link>
+                    </li>
+                ))}
             </ul>
         </nav>
     );
