696c1c5a1c8ee9d51439d8f237990eafd4cf083c
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-19 14:29:36 +0100

    change back card btn

--- theme.jsx
+++ theme.jsx
@@ -4,7 +4,7 @@ import { red } from '@mui/material/colors';
 const theme = createTheme({
     palette: {
         primary: {
-            main: '#556cd6'
+            main: '#272d2d'
         },
         secondary: {
             main: '#19857b'
