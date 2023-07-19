0392898987f69e6b2398d1568f21ccb24ede0ca3
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2023-07-19 13:37:09 +0100

    add tailwind

--- components/web-pages/About/About.jsx
+++ components/web-pages/About/About.jsx
@@ -11,7 +11,7 @@ const About = () => {
             />
             <div>
                 <h1 className="title">About</h1>
-                <p>
+                <p className="font-black">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                     Atque delectus deserunt distinctio dolore, facere illum
                     maxime molestias rerum tempora vitae? A alias cumque debitis
---
+++ tailwind.config.js
@@ -0,0 +1,5 @@
+// tailwind.config.js
+module.exports = {
+    purge: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}']
+    // ...other Tailwind CSS configurations...
+};
